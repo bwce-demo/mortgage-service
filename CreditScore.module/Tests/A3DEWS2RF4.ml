3051817a28e9f256f7e45a6f6522ced7009df01b
Rishi <rishi@demo.com>
2018-09-07 01:07:08 -0700

    Mortgage service

---
+++ CreditScore.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/CreditScore.module/Tests/TestLeads.bwt#/"/>
+  <EmulationDataFile>ACED0005740012436865636B53636F7265546573742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED000574000D546573744C656164732E627774</EmulationDataFile>
+</emulation:BWTFile>
